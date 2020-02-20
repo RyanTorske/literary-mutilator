00afe917cf27b5278cbd0eb86eef2671f8dcf4d9
Ryan Torske <rtorske@cnm.edu>
2020-02-20 15:46:28 -0700

    Added word replace on paragraph 2 from Captain to Mal.

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function changeColor() {
-	let p1 = document.getElementById("header");
+	let p = document.getElementById("header");
 	header.style.backgroundColor = "yellow";
 }
+
+function changeWord() {
+	let string = document.getElementById("second").innerHTML;
+	let change = string.replace("Captain", "Mal");
+	document.getElementById("second").innerHTML = change;
+}
+
