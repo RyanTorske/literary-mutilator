484f891360f1700f2336827c0a8b574e9004f51d
Ryan Torske <rtorske@cnm.edu>
2020-02-20 15:58:18 -0700

    Changed my changeWord section. Now it replaces whole section.

--- script.js
+++ script.js
@@ -3,9 +3,20 @@ function changeColor() {
 	header.style.backgroundColor = "yellow";
 }
 
+// function changeWord() {
+// 	let string = document.getElementById("second").innerHTML;
+// 	let change = string.replace("Captain", "Mal");
+// 	document.getElementById("second").innerHTML = change;
+// }
+
 function changeWord() {
 	let string = document.getElementById("second").innerHTML;
-	let change = string.replace("Captain", "Mal");
+	let change = string.replace("The Operative: You are fooling yourself, Captain. Nothing here is what it seems. You are\n" +
+		"\tnot the plucky hero, the Alliance is not an evil empire, and this is not the grand arena.  Inara Serra:\n" +
+		"\tAnd that's not incense.", "I have no clue what they are talking about.");
 	document.getElementById("second").innerHTML = change;
 }
 
+function wordColor() {
+	
+}
